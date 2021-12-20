d914216c46b1a25b876daf0811bcaa280a913679
JNYLee <junyjuny1021@gmail.com>
2021-12-20 17:59:40 +0900

    웹8버전 트위터 클론 완성

--- nwitter/src/index.js
+++ nwitter/src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from "./component/App";
-import { authService } from "./routes/fbase";
-console.log(authService);
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "components/App";
+import "./styles.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
--- nwitter/src/routes/Auth.js
+++ nwitter/src/routes/Auth.js
@@ -1,34 +1,44 @@
-import React, { useState } from "react";
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faGoogle,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+import { authService, firebaseInstance } from "fbase";
+import AuthForm from "components/AuthForm";
 
-const Auth = () =>{
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [newAccount, setNewAccount] = useState(false);
-    const onChange = (event) => {
-        const {target: {name, value}} = event;
-        if(name === "email") {
-            setEmail(value);
-        }else if(name ==="password") {
-
-        };
-    }; 
-        const onSubmit = async (event) => {
-            event.preventDefault();
-        }
-    return (
-        <div>
-            <form onSubmit={onSubmit}>
-                <input name ="email" type="text" value={email} placeholder="Email" required />
-                <input name ="password" type="password" placeholder="Password" value={password} onChange={onChange} required />
-                <input type="submit" value={newAccount ? "Create Account" : "Log In"} />
-            </form>
-    
-                <div>
-                    <button>Continue with Google</button>
-                    <button>Continue with Github</button>
-                </div>
-        
-        </div>
-    )
-} 
+const Auth = () => {
+  const onSocialClick = async (event) => {
+    const {
+      target: { name },
+    } = event;
+    let provider;
+    if (name === "google") {
+      provider = new firebaseInstance.auth.GoogleAuthProvider();
+    } else if (name === "github") {
+      provider = new firebaseInstance.auth.GithubAuthProvider();
+    }
+    await authService.signInWithPopup(provider);
+  };
+  return (
+    <div className="authContainer">
+      <FontAwesomeIcon
+        icon={faTwitter}
+        color={"#04AAFF"}
+        size="3x"
+        style={{ marginBottom: 30 }}
+      />
+      <AuthForm />
+      <div className="authBtns">
+        <button onClick={onSocialClick} name="google" className="authBtn">
+          Continue with Google <FontAwesomeIcon icon={faGoogle} />
+        </button>
+        <button onClick={onSocialClick} name="github" className="authBtn">
+          Continue with Github <FontAwesomeIcon icon={faGithub} />
+        </button>
+      </div>
+    </div>
+  );
+};
 export default Auth;
